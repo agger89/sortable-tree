5638268228c46fb2a62af01c76b4ba0da474b790
조충은 <agger2154@gmail.com>
2020-01-20 00:15:24 +0900

    배포 전 코드정리

--- src/treeData.tsx
+++ src/treeData.tsx
@@ -1,12 +1,6 @@
-import React from 'react';
 import  {
   TreeItem
 } from 'react-sortable-tree'
-
-const maxDepth = 5;
-
-const renderDepthTitle = ( path: any ) => `Depth: ${path.length}`;
-
 
 const treeData: TreeItem[] = [
   {
@@ -17,6 +11,7 @@ const treeData: TreeItem[] = [
       {
         title: 'Child Node',
         subtitle: 'Defined in `children` array belonging to parent',
+        expanded: false,
       },
     ],
   },
